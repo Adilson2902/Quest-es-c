ed66679bb4612af5566ccb710da726b0966dc903
Adilson2902 <38479849+Adilson2902@users.noreply.github.com>
2021-03-10 08:23:34 -0300

    exemplo2

--- timer/exemplostime.h
+++ timer/exemplostime.h
@@ -3,8 +3,20 @@ void  timeexemplo1(){
     time_t  seg;
 
     seg = time(NULL);
-    printf("N de horas que passou: %d",(seg/3600));
+    printf("N de horas que passou: %d \n",(seg/3600));
 
 }
 
+void timeexemplo2(){
+
+
+    time_t  seg;
+
+    seg = time(NULL);
+
+    printf(ctime(&seg));
+
+
+
+}
 
